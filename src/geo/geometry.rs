2c64faf5b76837e01ae8e63ac4529e6d4c7a8ceb
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-09-19 10:50:20 +0200

    geometry: add couple of helper functions

--- src/geo/geometry.rs
+++ src/geo/geometry.rs
@@ -8,9 +8,31 @@ impl Geometry {
         }
     }
 
+    pub fn from_polygons(polygons: Vec<Polygon>) -> Self {
+        Self {
+            polygons,
+            paths: vec![],
+        }
+    }
+
+    pub const fn from_paths(paths: Vec<Path>) -> Self {
+        Self {
+            polygons: vec![],
+            paths,
+        }
+    }
+
     pub fn extend(&mut self, o: &Self) {
         self.polygons.extend_from_slice(&o.polygons);
         self.paths.extend_from_slice(&o.paths);
+    }
+
+    pub fn polygons(&self) -> &[Polygon] {
+        &self.polygons
+    }
+
+    pub fn lines(&self) -> &[Path] {
+        &self.paths
     }
 }
 
