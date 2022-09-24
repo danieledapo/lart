ad7ca09f8e7adba1e02acdbc06a9e8f5d8a51654
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-09-24 14:28:50 +0200

    algo: add utilities

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -1,4 +1,30 @@
 pub mod spline;
 pub mod voro_tri;
 
+use std::f64::consts::TAU;
+
 pub use voro_tri::*;
+
+use crate::V;
+
+/// Return the `n` angles diving the circle in `n` arcs.
+pub fn polar_angles(n: u16) -> impl Iterator<Item = f64> {
+    (0..n).map(move |i| {
+        let t = f64::from(i) / f64::from(n);
+        t * TAU
+    })
+}
+
+/// Return an iterator over the positions in a `columns` x `rows` grid.
+///
+/// Note that the positions are normalized in [0..1]
+pub fn grid_positions(columns: u16, rows: u16) -> impl Iterator<Item = V> {
+    (0..rows).flat_map(move |r| {
+        (0..columns).map(move |c| {
+            V::new(
+                f64::from(c) / f64::from(columns),
+                f64::from(r) / f64::from(rows),
+            )
+        })
+    })
+}
