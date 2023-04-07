c763e3c0f22e209b8f7284487686763b05b05ecc
Daniele D'Orazio <d.dorazio96@gmail.com>
2023-04-07 17:26:24 +0200

    sketch/hatching: add horizontal and vertical hatching

--- src/sketch/hatching.rs
+++ src/sketch/hatching.rs
@@ -1,8 +1,12 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, ops::BitAnd};
 
-use crate::{frange, path, Bbox, Geometry, Polygon, V};
+use crate::{frange, path, Bbox, Geometry, V};
 
-pub fn parallel_hatch(g: &Polygon, a: f64, step: f64) -> Geometry {
+pub fn parallel_hatch<'a, T>(g: &'a T, a: f64, step: f64) -> Geometry
+where
+    T: Bbox,
+    Geometry: BitAnd<&'a T, Output = Geometry>,
+{
     let mut tex = Geometry::new();
     let Some(bbox) = g.bbox() else { return tex };
 
@@ -15,6 +19,36 @@ pub fn parallel_hatch(g: &Polygon, a: f64, step: f64) -> Geometry {
     for dd in frange(-r, r + step, step) {
         let o = pd * dd;
         tex.push_path(path!(p0 + o, p1 + o));
+    }
+
+    tex & g
+}
+
+pub fn vertical_hatch<'a, T>(g: &'a T, step: f64) -> Geometry
+where
+    T: Bbox,
+    Geometry: BitAnd<&'a T, Output = Geometry>,
+{
+    let mut tex = Geometry::new();
+    let Some(bbox) = g.bbox() else { return tex };
+
+    for x in frange(bbox.left(), bbox.right() + step, step) {
+        tex.push_path(path!((x, bbox.top()), (x, bbox.bottom())));
+    }
+
+    tex & g
+}
+
+pub fn horizontal_hatch<'a, T>(g: &'a T, step: f64) -> Geometry
+where
+    T: Bbox,
+    Geometry: BitAnd<&'a T, Output = Geometry>,
+{
+    let mut tex = Geometry::new();
+    let Some(bbox) = g.bbox() else { return tex };
+
+    for y in frange(bbox.top(), bbox.bottom() + step, step) {
+        tex.push_path(path!((bbox.left(), y), (bbox.right(), y)));
     }
 
     tex & g
