f283bb2df9d89e6f85dd6cbf4aa727fa9ed0f8b9
Daniele D'Orazio <d.dorazio96@gmail.com>
2023-12-30 23:04:56 +0100

    geo/utils: rework frange to return an iterator
    This allows us to optimize some iterator functions.

--- src/geo/utils.rs
+++ src/geo/utils.rs
@@ -2,20 +2,20 @@ use std::ops::{Add, Mul, Sub};
 
 /// Create an iterator over a range of floats stepping by the given amount.
 ///
+/// The range is exclusive with regard to the end.
+///
 /// ```rust
 /// # use lart::*;
 /// assert_eq!(frange(-1.5, 1.0, 0.5).collect::<Vec<_>>(), vec![-1.5, -1.0, -0.5, 0.0, 0.5]);
+/// assert_eq!(frange(1.5, -1.0, -0.5).collect::<Vec<_>>(), vec![1.5, 1.0, 0.5, 0.0, -0.5]);
+/// assert_eq!(frange(0.0, 0.1, 0.5).collect::<Vec<_>>(), vec![0.0]);
+/// assert_eq!(frange(0.0, -0.1, -0.5).collect::<Vec<_>>(), vec![0.0]);
 /// ```
-pub fn frange(mut start: f64, end: f64, step: f64) -> impl Iterator<Item = f64> {
-    std::iter::from_fn(move || {
-        if (end - start).abs() < step.abs() {
-            return None;
-        }
+pub fn frange(start: f64, end: f64, step: f64) -> FRange {
+    debug_assert!(step != 0.0);
+    debug_assert!((end - start).signum() == step.signum());
 
-        let v = start;
-        start += step;
-        Some(v)
-    })
+    FRange { start, end, step }
 }
 
 /// Linearly interpolate between the two given numbers or points with the given
@@ -88,5 +88,56 @@ impl Ord for F64Key {
 impl PartialOrd for F64Key {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
+    }
+}
+
+pub struct FRange {
+    start: f64,
+    end: f64,
+    step: f64,
+}
+
+impl Iterator for FRange {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if (self.step > 0.0 && self.start >= self.end)
+            || (self.step < 0.0 && self.start <= self.end)
+        {
+            return None;
+        }
+
+        let v = self.start;
+        self.start += self.step;
+        Some(v)
+    }
+
+    // The following functions rely on the fact that `sizeof::<usize>() >= sizeof::<f64>()`
+    // which is fine, I don't plan to support 32bit platforms nor 16bit ones.
+    #[cfg(target_pointer_width = "64")]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let n = f64::abs((self.end - self.start) / self.step).ceil();
+        (n as usize, Some(n as usize))
+    }
+
+    #[cfg(target_pointer_width = "64")]
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        self.size_hint().0
+    }
+
+    #[cfg(target_pointer_width = "64")]
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        if n < f64::MAX as usize {
+            self.start += self.step * n as f64;
+        } else {
+            for _ in 0..n {
+                self.next()?;
+            }
+        }
+
+        self.next()
     }
 }
